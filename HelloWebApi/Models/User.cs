f97820505e3dda73a3973535cc74c0b7f38773cb
Danil Vasilenko <danyav2@yandex.ru>
2021-05-28 21:22:48 +0400

    Добавьте файлы проекта.

---
+++ HelloWebApi/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
